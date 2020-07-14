265432bd38411c56898f805657ee05a0cdcc2930
kz <zaramaluna1999@hotmail.com>
2020-07-14 15:35:39 -0500

    project initiallized

---
+++ src/containers/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "../components/Header";
+import Search from "../components/Search";
+
+import "../assets/styles/App.scss";
+
+const App = () => <div className="App"></div>;
+
+export default App;
