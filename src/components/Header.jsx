2486c1414cfc69466fb4406a42f22052f056c113
kz <zaramaluna1999@hotmail.com>
2020-07-14 22:42:50 -0500

    images added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,29 @@
-import React from "react";
+import React from 'react';
 
-import "../assets/styles/components/Header.scss";
+import '../assets/styles/components/Header.scss';
+
+import logo from '../assets/static/logo-platzi-video-BW2.png';
+import userIcon from '../assets/static/user-icon.png';
 
 const Header = () => (
-  <header className="header">
+  <header className='header'>
     <img
-      className="header__img"
-      src="../assets/logo-video-BW2.png"
-      alt="React Video"
+      className='header__img'
+      src={logo}
+      alt='React Video'
     />
-    <div className="header__menu">
-      <div className="header__menu--profile">
-        <img src="../assets/user-icon.png" alt="" />
+    <div className='header__menu'>
+      <div className='header__menu--profile'>
+        <img src={userIcon} alt='' />
         <p>Perfil</p>
       </div>
 
       <ul>
         <li>
-          <a href="/">Cuenta</a>
+          <a href='/'>Cuenta</a>
         </li>
         <li>
-          <a href="/">Cerrar Sesión</a>
+          <a href='/'>Cerrar Sesión</a>
         </li>
       </ul>
     </div>
